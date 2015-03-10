9d20fd20d32145dcba4b6551b818135c9eff1075
Razvan Ciuca <razvann88@gmail.com>
2015-03-10 16:02:43 +0200

    fix .get to same endpoint conflict

--- app.js
+++ app.js
@@ -26,18 +26,18 @@ app.post('/api/users', user.createUser); //create a user
 app.put('/api/users/:id', user.editUser); //edit a user
 app.delete('/api/users/:id', user.deleteUser); //delete a user
 
-app.get('/api/rooms', room.getRooms); //get all users
+app.get('/api/rooms', room.getRooms); //get all rooms
+app.get('/api/rooms/:id([0-9]+)', room.getRoom); //get a specific room
 app.get('/api/rooms/search', room.search); //search for a room
-app.get('/api/rooms/:id', room.getRoom); //get a specific user
-app.post('/api/rooms', room.createRoom); //create a user
-app.put('/api/rooms/:id', room.editRoom); //edit a user
-app.delete('/api/rooms/:id', room.deleteRoom); //delete a user
+app.post('/api/rooms', room.createRoom); //create a room
+app.put('/api/rooms/:id', room.editRoom); //edit a room
+app.delete('/api/rooms/:id', room.deleteRoom); //delete a room
 
-app.get('/api/meetings', meeting.getMeetings); //get all users
-app.get('/api/meetings/:id', meeting.getMeeting); //get a specific user
-app.post('/api/meetings', meeting.createMeeting); //create a user
-app.put('/api/meetings/:id', meeting.editMeeting); //edit a user
-app.delete('/api/meetings/:id', meeting.deleteMeeting); //delete a user
+app.get('/api/meetings', meeting.getMeetings); //get all meetings
+app.get('/api/meetings/:id', meeting.getMeeting); //get a specific meeting
+app.post('/api/meetings', meeting.createMeeting); //create a meeting
+app.put('/api/meetings/:id', meeting.editMeeting); //edit a meeting
+app.delete('/api/meetings/:id', meeting.deleteMeeting); //delete a meeting
 
 app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'))
